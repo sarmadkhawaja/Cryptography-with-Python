8541521c371c80f33401c2086ed9a70c67c2fa99
sarmadkhawaja <45691159+sarmadkhawaja@users.noreply.github.com>
2019-03-23 13:53:33 +0000

    Delete Transposition_Cipher.py

--- Transposition_Cipher.py
+++
@@ -1,9 +0,0 @@
-def transpose(message, key):
-    ciphertext = ''
-    for i in range(key):        
-        for j in range(i, len(message), key):
-            ciphertext += message[j]
-    return ciphertext
-
-print(transpose('Common sense is not so common.', 8))
-
